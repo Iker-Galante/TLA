3a2d33c32973b7d1a52f0608bbe721754c8fe42f
Juan Bautista Albertoni Salini <juanbaalbertoni@gmail.com>
2025-05-15 20:07:57 -0300

    Grammar in progress

--- Flex-Bison-Compiler/src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
+++ Flex-Bison-Compiler/src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
@@ -18,7 +18,7 @@ typedef enum ExpressionType ExpressionType;
 typedef enum FactorType FactorType;
 
 typedef struct Constant Constant;
-typedef struct Expression Expression;
+/*typedef struct Expression Expression;*/
 typedef struct Factor Factor;
 typedef struct Program Program;
 
@@ -51,6 +51,7 @@ struct Factor {
 	FactorType type;
 };
 
+/*
 struct Expression {
 	union {
 		Factor * factor;
@@ -61,9 +62,462 @@ struct Expression {
 	};
 	ExpressionType type;
 };
+*/
+
+
+
+typedef enum ProgramType ProgramType;
+typedef enum HeaderFooterType HeaderFooterType;
+typedef enum BodyType BodyType;
+typedef enum ExpressionType ExpressionType;
+typedef enum SimpleExpressionType SimpleExpressionType;
+typedef enum ComplexExpressionType ComplexExpressionType;
+typedef enum TextType TextType;
+typedef enum ColorType ColorType;
+typedef enum ModifierType ModifierType;
+typedef enum HrefType HrefType;
+typedef enum PuntoPorPuntoType PuntoPorPuntoType;
+typedef enum SeccionType SeccionType;
+typedef enum TablaType TablaType;
+typedef enum NavegadorType NavegadorType;
+typedef enum FilaPPPType FilaPPPType;
+typedef enum ImgType ImgType;
+typedef enum TitleType TitleType;
+typedef enum SubtitleType SubtitleType;
+typedef enum LinkType LinkType;
+typedef enum FilaNavType FilaNavType;
+typedef enum FilaTablaType FilaTablaType;
+typedef enum ColumnaTablaType ColumnaTablaType;
+typedef enum ComponentType ComponentType;
+
+
+typedef struct Header Header;
+typedef struct Footer Footer;
+typedef struct Body Body;
+typedef struct Program Program;
+typedef struct Expression Expression;
+typedef struct SimpleExpression SimpleExpression;
+typedef struct Text Text;
+typedef struct Img Img;
+typedef struct Title Title;
+typedef struct Subtitle Subtitle;
+typedef struct Link Link;
+typedef struct Component Component;
+typedef struct Modifier Modifier;
+typedef struct ComplexExpression ComplexExpression;
+typedef struct Href Href;
+typedef struct Seccion Seccion;
+typedef struct Tabla Tabla;
+typedef struct Navegador Navegador;
+typedef struct PuntoPorPunto PuntoPorPunto;
+typedef struct FilaPPP FilaPPP;
+typedef struct FilaNav FilaNav;
+typedef struct FilaTabla FilaTabla;
+typedef struct ColumnaTabla ColumnaTabla;
+
+
+/**
+ * Node types for the Abstract Syntax Tree (AST).
+ */
+
+enum BodyType {
+	EXPRESSION,
+	EXPRESSION_BODY
+};
+
+enum HeaderFooterType {
+	BODY,
+	EMPTY
+};
+
+enum ProgramType {
+	EMPTY,
+	HEADER, 
+	FOOTER,
+	BODY,
+	HEADER_FOOTER,
+	HEADER_BODY,
+	FOOTER_BODY,
+	HEADER_FOOTER_BODY,
+};
+
+enum PuntoPorPuntoType {
+	FILA_PUNTO_POR_PUNTO,
+	EMPTY
+};
+
+enum ExpressionType {
+	SIMPLE_EXPRESSION,
+	COMPLEX_EXPRESSION,
+	COMPONENTE
+};
+
+enum SimpleExpressionType {
+	TEXT,
+	IMG,
+	TITLE,
+	SUBTITLE,
+	LINK
+};
+
+enum ComponentType {
+	COMPONENT,
+	EMPTY
+};
+
+enum ComplexExpressionType {
+	PUNTO_POR_PUNTO,
+	SECCION,
+	TABLA,
+	NAVEGADOR
+};
+
+enum ModifierType {
+	UNDERLINE,
+	BOLD,
+	ITALIC,
+	COLOR
+};
+
+enum SeccionType {
+	EMPTY,
+	BODY
+};
+
+enum TablaType {
+	EMPTY,
+	FILA_TABLA
+};
+enum FilaTablaType {
+	COL,
+	FILA_TABLA
+};
+
+enum NavegadorType {
+	EMPTY,
+	FILA_NAVEGADOR
+};
+
+enum FilaNavType {
+	FILA_NAVEGADOR,
+	EMPTY,
+};
+
+enum ColumnaTablaType {
+	COL,
+	FIN_FILA
+};
+
+enum ColorType {
+	RED,
+	GREEN,
+	BLUE,
+	YELLOW,
+	ORANGE,
+	PINK,
+	PURPLE,
+	BLACK,
+	WHITE
+};
+enum TextType {
+	MODIFIED_TEXT,
+	SIMPLE_TEXT
+};
+
+enum HrefType {
+	URL,
+	ID
+};
+enum ComplexExpressionType {
+	SECCION,
+	TABLA,
+	NAVEGADOR,
+	PUNTO_POR_PUNTO
+};
+
+enum FilaPPPType {
+	EXPRESSION_FILAPPP,
+	EXPRESSION
+};
+
+enum FilaNavType {
+	FILA_NAVEGADOR,
+	SIMPLE
+};
+
+
+///TODO esto esta incompleto, no se que hacer con las variabe.
+struct Expression {
+	union {
+		char * string;
+		// Previously declared component
+		Body * componentId;
+		struct {
+			SimpleExpression * simpleExpression;
+			char * id; //Esto sería variable. 
+		};
+		struct{
+			ComplexExpression * complexExpression;
+			char * id;
+		};
+		SimpleExpression * simpleExpression;
+		ComplexExpression * complexExpression;
+
+		//Component declaration
+		Component * component;
+	};
+	ExpressionType type;
+};
+
+struct SimpleExpression{
+	union{
+		Text * text;
+		Img * img;
+		Title * title;
+		Subtitle * subtitle;
+		Link * link;
+	};
+};
+
+struct ComplexExpression{
+	union{
+		PuntoPorPunto * puntoPorPunto;
+		Seccion * seccion;
+		Tabla * tabla;
+		Navegador * navegador;
+	};
+	ComplexExpressionType type;
+};
+
+struct Seccion{
+	union {
+		Body * body;
+		struct{};
+	};
+	SeccionType type;
+};
+
+struct Tabla{
+	union {
+		Body * body;
+		struct{};
+	};
+	TablaType type;
+};
+struct Navegador{
+	union {
+		FilaNav * filaNav;
+		struct{};
+	};
+	NavegadorType type;
+};
+
+struct PuntoPorPunto{
+	union{
+		FilaPPP * filaPPP;
+		struct{};
+	};
+	PuntoPorPuntoType type;
+};
+
+struct FilaPPP{
+	union{
+		struct {
+			FilaPPP * filaPPP;
+			Expression * expression;
+		};
+		Expression * expression;
+	};
+	FilaPPPType type;
+};
+
+struct Tabla{
+	union{
+		struct{};
+		FilaTabla * filaTabla;
+	};
+	TablaType type;
+};
+
+struct FilaTabla{
+	union{
+		struct {
+			FilaTabla * filaTabla;
+			ColumnaTabla * columnaTabla;
+		};
+		ColumnaTabla * columnaTabla;
+	};
+	FilaTablaType type;
+};
+
+struct ColumnaTabla{
+	union{
+		struct {
+			ColumnaTabla * columnaTabla;
+			SimpleExpression * expression;
+		};
+		struct{};
+	};
+	ColumnaTablaType type;
+};
+
+struct Navegador{
+	union{
+		FilaNav * filaNav;
+		struct{};
+	};
+	NavegadorType type;
+};
+
+struct FilaNav{
+	union{
+		struct{
+			char * string;
+			char * id;		
+			FilaNav * filaNav;
+		};
+		struct{
+			char * string;
+			char * id;
+		};
+	};
+	FilaNavType type;
+};
+
+//Ahhh creo que lo que habias hechoe staba bien. En el FilaNav
+
+struct Component{
+	union{
+		Body * body;
+		struct{};
+	};
+	char * id;
+	ComponentType type;
+};
+
+
+struct Img {
+	
+		char * url; //Variable
+		char * alternative;
+	
+};
+struct Title {
+		char * string;
+};
+
+struct Subtitle {
+		char * string;
+};
+
+
+
+struct Link {
+		char * href; //Variable
+		SimpleExpression * simpleExpression;
+};
+
+struct Href{
+	union {
+		char * url;//variable*/
+		char * id;//variable*/
+	};
+	HrefType type;
+};
+
+struct Text {
+	union {
+		struct {
+			char * string;
+			Modifier * modifier;
+		};
+		char * string;
+	};
+	TextType type;
+};
+
+
+struct Header {
+	union {
+		Body * body;
+		struct{};
+	};
+	HeaderFooterType type;
+};
+
+struct Footer {
+	union {
+		Body * body;
+		struct{};
+	};
+	HeaderFooterType type;
+};
+
+struct Body {
+	union {
+		Expression * expression;
+		struct {
+			Expression * expression;
+			Body * body;
+		};
+	};
+	BodyType type;
+};
+
+struct Modifier {
+	union {
+		struct {
+			ColorType * color;
+			Modifier * modifier;
+		};
+		struct {
+			Modifier * modifier;
+		};
+		struct{};
+	};
+	ModifierType * modifier;
+};
+
 
 struct Program {
-	Expression * expression;
+	union{
+		// Header, Body and Footer
+		struct {
+			Header * header;
+			Body * body;
+			Footer * footer;	
+		};
+		//Footer and Body
+		struct{
+			Body * body;
+			Footer * footer;
+		};
+		// Header and Body
+		struct{
+			Header * header;
+			Body * body;
+		};
+		// Header and Footer
+		struct{
+			Header * header;
+			Footer * footer;
+		};
+		// Header
+		struct{
+			Header * header;
+		};
+		// Body
+		struct{
+			Body * body;
+		};
+		//Footer
+		struct{
+			Footer * footer;
+		};
+		// Empty
+		struct{};
+	};
+	ProgramType type;
 };
 
 /**
@@ -73,5 +527,14 @@ void releaseConstant(Constant * constant);
 void releaseExpression(Expression * expression);
 void releaseFactor(Factor * factor);
 void releaseProgram(Program * program);
+void releaseSimpleExpression(SimpleExpression * simpleExpression);
+void releaseComplexExpression(ComplexExpression * complexExpression);
+void releaseText(Text * text);
+void releaseImg(Img * img);
+void releaseTitle(Title * title);
+void releaseSubtitle(Subtitle * subtitle);
+void releaseLink(Link * link);
+void releaseComponent(Component * component);
+void releaseModifier(Modifier * modifier);
 
 #endif
--- Flex-Bison-Compiler/src/main/c/frontend/syntactic-analysis/BisonGrammar.y
+++ Flex-Bison-Compiler/src/main/c/frontend/syntactic-analysis/BisonGrammar.y
@@ -10,15 +10,30 @@
 %union {
 	/** Terminals. */
 
-	int integer;
+	char * string;
+       char * id;
 	Token token;
 
 	/** Non-terminals. */
 
-	Constant * constant;
-	Expression * expression;
-	Factor * factor;
 	Program * program;
+	Header * header;
+	Body * body;
+	Footer * footer;
+	SimpleExpression * simple_expression;
+	Modifiers * modifiers;
+	Component * component;
+	ComplexExpression * complex_expression;
+	RowPPP * row_ppp;
+	RowTable * row_table;
+	ColumnTable * column_table;
+	RowNav * row_nav;
+	Section * section;
+	Expression * expression;
+	Text * text;
+	Image * image;
+	Title * title;
+	Subtitle * subtitle;
 }
 
 /**
@@ -34,49 +49,123 @@
 %destructor { releaseFactor($$); } <factor>
 
 /** Terminals. */
-%token <integer> INTEGER
-%token <token> ADD
-%token <token> CLOSE_PARENTHESIS
-%token <token> DIV
-%token <token> MUL
-%token <token> OPEN_PARENTHESIS
-%token <token> SUB
-
+%token <string> STRING
+%token <id> ID
+%token <token> PRINCIPIO FIN ENCABEZADO PIE TEXTO IMAGEN TITULO SUBTITULO ENLACE COLOR SUBRAYADO ITALICA NEGRITA TAMANIO
+%token <token> PUNTO_POR_PUNTO SECCION TABLA NAVEGADOR COMPONENTE
+%token <token> INICIO_TABLA FIN_TABLA INICIO_SECCION FIN_SECCION FIN_ENCABEZADO FIN_PIE FIN_COMPONENTE FIN_FILA FIN_PPP FIN_NAV
 %token <token> UNKNOWN
 
 /** Non-terminals. */
-%type <constant> constant
-%type <expression> expression
-%type <factor> factor
 %type <program> program
+%type <header> header
+%type <body> body
+%type <footer> footer
+%type <expression> expression
+%type <simple_expression> simple_expression
+%type <modifiers> modifiers
+%type <component> component
+%type <complex_expression> complex_expression
+%type <row_ppp> row_ppp
+%type <row_table> row_table
+%type <column_table> column_table
+%type <row_nav> row_nav
+%type <section> section
+%type <text> text
+%type <image> image
+%type <title> title
+%type <subtitle> subtitle
 
 /**
  * Precedence and associativity.
  *
  * @see https://www.gnu.org/software/bison/manual/html_node/Precedence.html
  */
-%left ADD SUB
-%left MUL DIV
 
 %%
 
 // IMPORTANT: To use λ in the following grammar, use the %empty symbol.
 
-program: expression													{ $$ = ExpressionProgramSemanticAction(currentCompilerState(), $1); }
-	;
 
-expression: expression[left] ADD expression[right]					{ $$ = ArithmeticExpressionSemanticAction($left, $right, ADDITION); }
-	| expression[left] DIV expression[right]						{ $$ = ArithmeticExpressionSemanticAction($left, $right, DIVISION); }
-	| expression[left] MUL expression[right]						{ $$ = ArithmeticExpressionSemanticAction($left, $right, MULTIPLICATION); }
-	| expression[left] SUB expression[right]						{ $$ = ArithmeticExpressionSemanticAction($left, $right, SUBTRACTION); }
-	| factor														{ $$ = FactorExpressionSemanticAction($1); }
-	;
+program: PRINCIPIO header body footer FIN { $$ = ProgramSemanticAction($2, $3, $4); }
+       | PRINCIPIO body footer FIN { $$ = ProgramSemanticAction(NULL, $2, $3); }
+       | PRINCIPIO header footer FIN { $$ = ProgramSemanticAction($2, NULL, $3); }
+       | PRINCIPIO header body FIN { $$ = ProgramSemanticAction($2, $3, NULL); }
+       | PRINCIPIO footer FIN { $$ = ProgramSemanticAction(NULL, NULL, $2); }
+       | PRINCIPIO body FIN { $$ = ProgramSemanticAction(NULL, $2, NULL); }
+       | PRINCIPIO header FIN { $$ = ProgramSemanticAction($2, NULL, NULL); }
+       | PRINCIPIO FIN { $$ = ProgramSemanticAction(NULL, NULL, NULL); }
+       ;
 
-factor: OPEN_PARENTHESIS expression CLOSE_PARENTHESIS				{ $$ = ExpressionFactorSemanticAction($2); }
-	| constant														{ $$ = ConstantFactorSemanticAction($1); }
-	;
+header: ENCABEZADO ':' '\n' body FIN_ENCABEZADO '\n' { $$ = HeaderSemanticAction($4); }
+      | ENCABEZADO ':' '\n' FIN_ENCABEZADO '\n' { $$ = HeaderSemanticAction(NULL); }
+      ;
 
-constant: INTEGER													{ $$ = IntegerConstantSemanticAction($1); }
-	;
+footer: PIE ':' '\n' body FIN_PIE '\n' { $$ = FooterSemanticAction($4); }
+      | PIE ':' '\n' FIN_PIE '\n' { $$ = FooterSemanticAction(NULL); }
+      ;
+
+body: expression body { $$ = BodySemanticAction($1, $2); }
+    | expression { $$ = BodySemanticAction($1, NULL); }
+    ;
+
+expression: '(' ID ')' simple_expression { $$ = IdSimpleExpressionSemanticAction($2, $4); }
+          | '(' ID ')' complex_expression { $$ = IdComplexExpressionSemanticAction($2, $4); }
+          | STRING { $$ = StringExpressionSemanticAction($1); }
+          | simple_expression { $$ = SimpleExpressionSemanticAction($1); }
+          | '{' ID '}' { $$ = IdExpressionSemanticAction($2); }
+          | component { $$ = ComponentExpressionSemanticAction($1); }
+          | complex_expression { $$ = ComplexExpressionSemanticAction($1); }
+          ;
+
+simple_expression: TEXTO { $$ = TextSemanticAction($1); }
+                 | IMAGEN { $$ = ImageSemanticAction($1); }
+                 | TITULO { $$ = TitleSemanticAction($1); }
+                 | SUBTITULO { $$ = SubtitleSemanticAction($1); }
+                 | ENLACE { $$ = LinkSemanticAction($1); }
+                 ;
+
+modifiers: modifiers COLOR { $$ = AddModifierSemanticAction($1, COLOR); }
+         | modifiers SUBRAYADO { $$ = AddModifierSemanticAction($1, SUBRAYADO); }
+         | modifiers ITALICA { $$ = AddModifierSemanticAction($1, ITALICA); }
+         | modifiers NEGRITA { $$ = AddModifierSemanticAction($1, NEGRITA); }
+         | modifiers TAMANIO { $$ = AddModifierSemanticAction($1, TAMANIO); }
+         | COLOR { $$ = SingleModifierSemanticAction(COLOR); }
+         | SUBRAYADO { $$ = SingleModifierSemanticAction(SUBRAYADO); }
+         | ITALICA { $$ = SingleModifierSemanticAction(ITALICA); }
+         | NEGRITA { $$ = SingleModifierSemanticAction(NEGRITA); }
+         | TAMANIO { $$ = SingleModifierSemanticAction(TAMANIO); }
+         ;
+
+component: COMPONENTE '(' ID ')' '\n' body FIN_COMPONENTE '\n' { $$ = ComponentSemanticAction($3, $6); }
+         | COMPONENTE '(' ID ')' '\n' FIN_COMPONENTE '\n' { $$ = ComponentSemanticAction($3, NULL); }
+         ;
+
+complex_expression: PUNTO_POR_PUNTO { $$ = PuntoPorPuntoSemanticAction(); }
+                  | SECCION { $$ = SectionSemanticAction(); }
+                  | TABLA { $$ = TableSemanticAction(); }
+                  | NAVEGADOR { $$ = NavigatorSemanticAction(); }
+                  ;
+
+row_ppp: '-' expression row_ppp                  { $$ = RowPPPSemanticAction($2, $3); }
+       | '-' expression                          { $$ = RowPPPSemanticAction($2, NULL); }
+       | '-' row_ppp                                    { $$ = RowPPPSemanticAction(NULL, $2); }
+       ;
+
+row_table: column_table row_table                       { $$ = RowTableSemanticAction($1, $2); }
+         | column_table                                 { $$ = RowTableSemanticAction($1, NULL); }
+         ;
+
+column_table: simple_expression column_table { $$ = ColumnTableSemanticAction($1, $2); }
+            | FIN_FILA { $$ = ColumnTableSemanticAction(NULL, NULL); }
+            ;
+
+row_nav: '-' '(' ID ')' STRING '\n' row_nav { $$ = RowNavSemanticAction($3, $5, $7); }
+       | '-' '(' ID ')' STRING '\n' { $$ = RowNavSemanticAction($3, $5, NULL); }
+       ;
+
+section: INICIO_SECCION ':' '\n' body FIN_SECCION '\n' { $$ = SectionSemanticAction($4); }
+       | INICIO_SECCION ':' '\n' FIN_SECCION '\n' { $$ = SectionSemanticAction(NULL); }
+       ;
 
 %%
