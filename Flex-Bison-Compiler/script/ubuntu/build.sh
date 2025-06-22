58506bd1a75fcb7c09542ddb77e7c8460965bfbd
Iker-Galante <igalante@itba.edu.ar>
2025-06-22 19:13:30 -0300

    So the results folder creates when build

--- Flex-Bison-Compiler/script/ubuntu/build.sh
+++ Flex-Bison-Compiler/script/ubuntu/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 BASE_PATH="$(dirname "$0")/../.."
 cd "$BASE_PATH"
-
+mkdir results
 rm --force --recursive "$BASE_PATH/build"
 rm --force "$BASE_PATH/src/main/c/frontend/lexical-analysis/FlexScanner.c"
 rm --force "$BASE_PATH/src/main/c/frontend/syntactic-analysis/BisonParser.c"
